06376b696ea362309d5e8dd1cb796dd958f0aa9d
utkarshMAVcode <utkarsh.mission@gmail.com>
2023-06-05 21:44:43 +0530

    leetcode 832

---
+++ leetcode832.cpp
@@ -0,0 +1,17 @@
+// 832. flipping an image
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        for(int i=0;i<image.size();i++){
+            for(int j=0;j<image[i].size();j++){
+                if(image[i][j]==0){
+                    image[i][j]=1;
+                }else{
+                    image[i][j]=0;
+            }
+            }
+        reverse(image[i].begin(),image[i].end());
+        }
+        return image;
+    }
+};
