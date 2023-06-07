16f678f37eeb2a4fd9816d9f2983018747facf9a
utkarshMAVcode <utkarsh.mission@gmail.com>
2023-06-07 21:43:34 +0530

    leetcode 1431 comment

--- leetcode1431.cpp
+++ leetcode1431.cpp
@@ -1,1 +1,4 @@
 // 1431. kids with greatest 
+// this question is now in leetcode premium now
+// i have solved this question but can't access my solution
+// or the question now unless i buy premium
