ec79eb3e7e7a3ed7ccf6df47c799b7be12cfd939
utkarshMAVcode <utkarsh.mission@gmail.com>
2023-06-08 14:07:24 +0530

    leetcode 1910

---
+++ leetcode1910.cpp
@@ -0,0 +1,10 @@
+// 1910. remove all occurences of a substring
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        while(s.size()!=0 && s.find(part)<s.size()){
+            s.erase(s.find(part),part.size());
+        }
+        return s;
+    }
+};
